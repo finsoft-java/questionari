89067a77df046273f52a16dddb13d3831aca5bb7
Luca Vercelli <luca.vercelli.to@gmail.com>
2019-08-28 15:55:41 +0200

    search utenti

--- questionari/src/app/utenti/utenti.component.ts
+++ questionari/src/app/utenti/utenti.component.ts
@@ -2,6 +2,7 @@
 import { Subscription, Observable } from 'rxjs';
 import { User } from '@/_models';
 import { UserService, AuthenticationService, AlertService } from '@/_services';
+import { map } from 'rxjs/operators';
 
 @Component({templateUrl: 'utenti.component.html'})
 export class UtentiComponent implements OnInit, OnDestroy {
@@ -10,6 +11,11 @@ export class UtentiComponent implements OnInit, OnDestroy {
     utenti : User[];
     editing : boolean = false;
     message : string;
+    searchString : string;
+    previousSearch : string;
+    timeout : any; // NodeJS.Timeout ?!?
+    utenti_visibili : User[];
+
     constructor(
         private authenticationService: AuthenticationService,
         private userService: UserService,
@@ -42,16 +48,48 @@ export class UtentiComponent implements OnInit, OnDestroy {
       this.userService.getAll()
         .subscribe(response => {
             this.utenti = response["data"];
+            this._calcola_utenti_visibili();
         });
+    }
+    _calcola_utenti_visibili() {
+        if (!this.searchString) {
+            this.utenti_visibili = this.utenti;
+        } else {
+            this.utenti_visibili = this.utenti.filter(user => 
+                (user.username != null && user.username.includes(this.searchString)) ||
+                (user.cognome != null && user.cognome.includes(this.searchString)) ||
+                (user.nome != null && user.nome.includes(this.searchString)) ||
+                (user.email != null && user.email.includes(this.searchString))
+            );
+        }
     }
     removeItem(index: number) {
       this.utenti.splice(index, 1);
+      this._calcola_utenti_visibili();
     }
     refresh() {
         this.getUsers();
     }
     sync() {
         this.alertService.error("Non implementato");
+    }
+    /**
+     * Se l'utente scrive "Paperino", aspettiamo mezzo secondo dall'ultima lettera
+     * e poi lanciamo la ricerca una volta sola
+     */
+    wait_then_refresh($event) {
+        if (this.previousSearch == this.searchString) {
+            // L'utente ha schiacciato CTRL, ENTER, SHIFT, ...
+            return;
+        }
+        this.previousSearch = this.searchString;
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+        }
+        this.timeout = setTimeout(() => {
+            this.timeout = null;
+            this._calcola_utenti_visibili();
+        }, 500);
     }
 
 }
