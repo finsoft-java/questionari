6fc32c9161efb1ec5840f3ea6479ecc4190582bb
Luca Vercelli <luca.vercelli.to@gmail.com>
2019-08-30 09:59:31 +0200

    comment

--- ws/include/all.php
+++ ws/include/all.php
@@ -2,6 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: OPTIONS,GET,PUT,POST,DELETE");
 header("Access-Control-Allow-Headers: Accept,Content-Type,Authorization");
+# Chrome funziona anche con Access-Control-Allow-Headers: *  invece Firefox no
 
 #costants
 include("config.php");
