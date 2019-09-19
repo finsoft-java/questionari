3caf3848475492da109e880c1adac9a23f42008b
Luca Vercelli <luca.vercelli.to@gmail.com>
2019-09-19 12:19:27 +0200

    etichette

--- questionari/webpack.dev.js
+++ questionari/webpack.dev.js
@@ -15,11 +15,11 @@ module.exports = merge(common, {
             // global app config object
             config: JSON.stringify({
                 //apiUrl: 'http://192.168.99.100/ws',                   //luca docker
-                //apiUrl: 'http://localhost/ws',                        //luca
-                apiUrl: 'http://127.0.0.1/questionari/ws',              // ale
+                apiUrl: 'http://localhost/ws',                        //luca
+                //apiUrl: 'http://127.0.0.1/questionari/ws',              // ale
                 //websocketUrl: 'ws://192.168.99.100:9000'              //luca docker
-                //websocketUrl: 'ws://localhost:9000'                   //luca
-                websocketUrl: 'ws://127.0.0.1:9000'                     //ale
+                websocketUrl: 'ws://localhost:9000'                  //luca
+                //websocketUrl: 'ws://127.0.0.1:9000'                    //ale
             })
         })
     ],
