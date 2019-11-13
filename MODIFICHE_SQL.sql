b765d4e96fd47a1c4870f38fc487b45dbc8aed3f
Alessandro <alessandrobarsanti6@gmail.com>
2019-11-13 15:32:05 +0100

    fix bug

--- MODIFICHE_SQL.sql
+++ MODIFICHE_SQL.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `domande` CHANGE `descrizione` `descrizione` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE `risposte_ammesse` CHANGE `descrizione` `descrizione` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+--metodo più veloce per il controllo sull'email duplicata
+ALTER TABLE `utenti` CHANGE `email` `email` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+ALTER TABLE `questionari`.`utenti` ADD UNIQUE `email` (`email`);
