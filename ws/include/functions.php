b444082503f52e17f7d9650e7b8db135d86a9f43
alessandrobarsanti <alessandrobarsanti6@gmail.com>
2023-02-20 11:01:37 +0100

    aggiornamento php8

--- ws/include/functions.php
+++ ws/include/functions.php
@@ -2,7 +2,7 @@
 
 function connect() {
   $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-  if (mysqli_connect_errno($connect)) {
+  if (mysqli_connect_errno()) {
     die("Failed to connect:" . mysqli_connect_error());
   }
   mysqli_set_charset($connect, "utf8");
